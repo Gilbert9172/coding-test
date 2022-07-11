f3b9c0d308f139fe9a4ca511662b2e66f49836d2
gilbert9172 <gilbert9172@naver.com>
2022-07-12 00:13:42 +0900

    카카오 : 크레인 인형뽑기 (완료)

--- programmers/levelOne/quiz05.java
+++ programmers/levelOne/quiz05.java
@@ -1,46 +1,46 @@
 package levelOne;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class quiz05 {
     public static void main(String[] args) {
 
-        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
         int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
 
-
         int cnt = 0;
-        List<Integer> trash = new ArrayList<>();
-        if (trash.get(trash.size() - 1) == trash.get(trash.size() - 2)) {
-            cnt += 2;
-        }
-
+        Stack<Integer> trash = new Stack<>();
 
         for (int move : moves) {
 
+            if (trash.size() > 1) {
+                if (trash.get(trash.size() - 1) == trash.get(trash.size() - 2)) {
+                    trash.pop();
+                    trash.pop();
+                    cnt += 2;
+                }
+            }
             int ind = move - 1;
 
-            List<Integer> lst = new ArrayList<>();
+            ArrayList<Integer> lst = new ArrayList<>();
             for (int[] subList : board) {
                 int i = subList[ind];
                 lst.add(i);
             }
 
             for (int j = 0; j < lst.size(); j++) {
-
                 if (lst.get(j) > 0) {
-                    int initialValue = lst.get(j);
+                    trash.push(lst.get(j));
                     lst.set(j, 0);
-
-                    int test = 0;
-                    board[j][ind] = test;
-                    trash.add(initialValue);
+                    board[j][ind] = 0;
                     break;
                 }
             }
         }
         System.out.println(cnt);
-
     }
 }
+
+
+
