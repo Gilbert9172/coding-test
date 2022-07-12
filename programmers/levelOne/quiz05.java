c94e9d1206a2e0504aeb8af1ed6e231e619f55ba
gilbert9172 <gilbert9172@naver.com>
2022-07-12 22:00:18 +0900

    카카오 : 크레인 인형뽑기 (수정)

--- programmers/levelOne/quiz05.java
+++ programmers/levelOne/quiz05.java
@@ -3,9 +3,13 @@ package levelOne;
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * 2019 카카오 개발자 겨울 인턴십
+ * 크레인 인형 뽑기 게임 (Lv.1)
+ */
 public class quiz05 {
-    public static void main(String[] args) {
 
+    public static Integer Solution() {
         int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
         int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
 
@@ -32,13 +36,16 @@ public class quiz05 {
             for (int j = 0; j < lst.size(); j++) {
                 if (lst.get(j) > 0) {
                     trash.push(lst.get(j));
-                    lst.set(j, 0);
                     board[j][ind] = 0;
                     break;
                 }
             }
         }
-        System.out.println(cnt);
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(quiz05.Solution());
     }
 }
 
