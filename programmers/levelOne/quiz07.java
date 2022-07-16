a430e5852b0c1bd6a6c6b7d22e3f107e9f3ed9dd
gilbert9172 <gilbert9172@naver.com>
2022-07-16 18:33:00 +0900

    프로그래머스 : 포켓몬

---
+++ programmers/levelOne/quiz07.java
@@ -0,0 +1,43 @@
+package levelOne;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 알고리즘 : 해쉬
+ * 포켓몬
+ */
+
+public class quiz07 {
+    static int[] nums = {3, 1, 2, 3};
+    static Set<Integer> types = new HashSet<>();
+
+    public static void main(String[] args) {
+        System.out.println(quiz07.solution());
+    }
+
+    public static Integer solution() {
+        arrayToSet(nums);
+        return countPossibleTypes();
+    }
+
+    // int[] → HashSet<>
+    public static void arrayToSet(int[] nums) {
+        for (int num : nums) {
+            types.add(num);
+        }
+    }
+
+    // 최대 선택 종류 수.
+    public static Integer countPossibleTypes() {
+        int selectPocketmon = nums.length / 2;
+
+        int answer = 0;
+        if (selectPocketmon <= types.size()) {
+            answer += selectPocketmon;
+            return answer;
+        }
+        answer += types.size();
+        return answer;
+    }
+}
