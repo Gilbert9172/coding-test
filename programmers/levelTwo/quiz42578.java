f94d6f4fea55f837b67a5011f9c6d441ba8731bc
gilbert9172 <gilbert9172@naver.com>
2022-08-02 23:48:16 +0900

    위장

---
+++ programmers/levelTwo/quiz42578.java
@@ -0,0 +1,29 @@
+package levelTwo;
+
+import java.util.HashMap;
+
+/**
+ * 프로그래머스
+ * 해시(Lv.2)
+ * 위장
+ */
+public class quiz42578 {
+
+    public static void main(String[] args) {
+
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        HashMap<String, Integer> clothesMap = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            String type = clothe[1];
+            clothesMap.put(type, clothesMap.getOrDefault(type, 1)+ 1);
+        }
+
+        Integer answer = 1;
+        for (Integer value : clothesMap.values()) {
+            answer *= value;
+        }
+
+        System.out.println(answer-1);
+    }
+}
