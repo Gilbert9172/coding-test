315e54271a18d1dff5f4b07c735478116807daab
gilbert9172 <gilbert9172@naver.com>
2022-08-23 23:43:19 +0900

    프로그래머스 - 타겟넘버
    - 다시 풀기

---
+++ programmers/levelTwo/quiz43165.java
@@ -0,0 +1,28 @@
+package levelTwo;
+
+/**
+ * 타겟 넘버 (DFS/BFS) - DFS 사용
+ */
+public class quiz43165 {
+
+    static int[] numbers = {4, 1, 2, 1};
+    static int target = 4;
+    static int answer = 0;
+
+    public static void main(String[] args) {
+        dfs(0, 0);
+        System.out.println(answer);
+    }
+
+    public static void dfs(int index, int sum) {
+        //1. 탈출 조건
+        if (index == numbers.length) {
+            if (sum == target) answer++;
+            return;
+        }
+
+        //2. 수행 동작
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+}
