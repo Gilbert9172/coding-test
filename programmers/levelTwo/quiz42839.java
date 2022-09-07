b5b13eb48076f8bf32bad073aecc4fc5ce92f0c8
gilbert9172 <gilbert9172@naver.com>
2022-09-08 00:12:19 +0900

    프로그래머스 - 소수찾기
    - 에라토네스의 체
    - 순열 알고리즘(DFS)

---
+++ programmers/levelTwo/quiz42839.java
@@ -0,0 +1,79 @@
+package levelTwo;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+/**
+ * 프린터
+ */
+public class quiz42839 {
+
+    static HashSet<String> setList = new HashSet<>();
+    static int answer = 0;
+
+    public static void main(String[] args) {
+
+        String numbers = "17";
+
+        // String → int[]
+        int[] nums = Arrays.stream(numbers.split("")).mapToInt(Integer::parseInt).toArray();
+
+        // 순열
+        for (int i = 1; i <= nums.length; i++) {
+            permutation(nums, new int[nums.length], new boolean[nums.length], 0, 2);
+        }
+
+        // 소수 찾기
+        for (String element : setList) {
+            if (isPrimeNum(Integer.parseInt(element))) {
+                answer ++;
+            }
+        }
+
+        // 결과
+        System.out.println(answer);
+
+    }
+
+    // DFS → 주어진 수로 만들수 있는 모든 수.(순열)
+    public static void permutation(int[] nums, int[] output, boolean[] visited, int depth, int r) {
+
+        if (depth == r) {
+
+            int[] result = Arrays.copyOfRange(output, 0, depth);
+
+            String combinedNum = Arrays.stream(result).mapToObj(String::valueOf).collect(Collectors.joining());
+
+            if (!combinedNum.startsWith("0")) {
+                setList.add(combinedNum);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (!visited[i]) {
+                visited[i] = true;
+                output[depth] = nums[i];
+                permutation(nums, output, visited, depth + 1, r);
+                visited[i] = false;
+            }
+        }
+    }
+
+    // 소수확인
+    public static boolean isPrimeNum(int num) {
+
+        if (num == 0 || num == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
