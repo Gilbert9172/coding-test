49a28a511bba67f90c7c2d779f1d1dcddd517fe3
gilbert9172 <gilbert9172@naver.com>
2022-10-01 23:47:05 +0900

    프로그래머스(Lv.1) - 소수찾기

---
+++ programmers/levelOne/소수찾기.java
@@ -0,0 +1,38 @@
+package levelOne;
+
+public class 소수찾기 {
+
+    public static void main(String[] args) {
+        int[] prime = isPrime(10);
+        int count = 0;
+        for (int i : prime) {
+            if (i != 0) {
+                count ++;
+            }
+        }
+        System.out.println(count);
+
+    }
+
+
+    private static int[] isPrime(int num) {
+
+        int[] arr = new int[num + 1];
+
+        for (int i = 2; i <= num; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+
+            if (arr[i] == 0) {
+                continue;
+            }
+
+            for (int j = i * i; j <= num; j += i) {
+                arr[j] = 0;
+            }
+        }
+        return arr;
+    }
+}
