4af53ee97421c76bb6154a663929170a170d58d6
gilbert9172 <gilbert9172@naver.com>
2024-07-22 17:58:18 +0900

    leet code : 두수의 합

---
+++ codingInterviewBook/선형자료구조/배열/TwoSum.java
@@ -0,0 +1,59 @@
+package 선형자료구조.배열;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * <a href="https://leetcode.com/problems/two-sum/description">리트 코드 : 두수의 합</a>
+ * */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] initArray = {2, 6, 11, 15};
+        int target = 8;
+        int[] resultArray = solve2(initArray, target);
+        for (int i : resultArray) {
+            System.out.println(i);
+        }
+    }
+
+    public static int[] solve1(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // No solution found
+    }
+
+    public static int[] solve2(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numsMap.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (numsMap.containsKey(target - nums[i]) && i != numsMap.get(target - nums[i])) {
+                return new int[]{i, numsMap.get(target-nums[i])};
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] solve3(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
