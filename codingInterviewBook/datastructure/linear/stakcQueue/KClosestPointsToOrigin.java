eb9716150f529e271c86cd01d82b40443fd9b5fe
gilbert9172 <gilbert9172@naver.com>
2024-08-07 19:59:11 +0900

    [LeetCode#973] K Closest Points to Origin
    * comparator 익명 객체로 선언하여 코드 수정

--- codingInterviewBook/datastructure/linear/stakcQueue/KClosestPointsToOrigin.java
+++ codingInterviewBook/datastructure/linear/stakcQueue/KClosestPointsToOrigin.java
@@ -9,24 +9,51 @@ import java.util.PriorityQueue;
  */
 public class KClosestPointsToOrigin {
 
+    public static class Point {
+        long distance;
+        int[] point;
+
+        private Point(long distance, int[] point) {
+            this.distance = distance;
+            this.point = point;
+        }
+
+    }
+
     public int[][] kClosest(int[][] points, int k) {
-        Comparator<int[]> distanceComparator = (p1, p2) -> getDistance(p1).compareTo(getDistance(p2));
-        PriorityQueue<int[]> pq = new PriorityQueue<>(distanceComparator);
+
+        PriorityQueue<Point> pointPQ = new PriorityQueue<>(pointComparator);
 
         for(int[] point : points) {
-            pq.add(point);
+            pointPQ.add(new Point(calculateDistance(point), point));
         }
 
         int[][] result = new int[k][2];
 
         for(int i = 0; i < k; i++) {
-            result[i] = pq.poll();
+            if (pointPQ.isEmpty()) {
+                return result;
+            }
+            Point target = pointPQ.poll();
+            result[i] = target.point;
         }
-
         return result;
     }
 
-    private Long getDistance(int[] point) {
+    private Long calculateDistance(int[] point) {
         return ((long) point[0] * point[0]) + ((long) point[1] * point[1]);
+    }
+
+    public Comparator<Point> pointComparator = new Comparator<Point>() {
+        @Override
+        public int compare(Point p1, Point p2) {
+            if (p1.distance == p2.distance) {
+                return 0;
+            } else if (p1.distance > p2.distance) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
     }
 }
