ad0e6f365ef6867ed5fbff8ea3ab62a3fa119cfa
gilbert9172 <gilbert9172@naver.com>
2024-08-21 16:03:22 +0900

    [LeetCode#46] Permutations

---
+++ codingInterviewBook/datastructure/non_linear/graph/Permutations.java
@@ -0,0 +1,31 @@
+package datastructure.non_linear.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        List<Integer> elements = Arrays.stream(nums).boxed().toList();
+        dfs(results, new ArrayList<>(), elements);
+        return results;
+    }
+
+    private void dfs(List<List<Integer>> results, List<Integer> prevElement, List<Integer> elements) {
+        if (elements.isEmpty()) {
+            results.add(prevElement.stream().toList());
+        }
+
+        for (Integer e : elements) {
+            List<Integer> nextElements = new ArrayList<>(elements);
+            nextElements.remove(e);
+
+            prevElement.add(e);
+            dfs(results, prevElement, nextElements);
+            prevElement.remove(e);
+        }
+    }
+
+}
