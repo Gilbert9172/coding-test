e0722a9cab9d72c2912fb6f21b546576ee0f17d7
gilbert9172 <gilbert9172@naver.com>
2024-08-30 22:14:07 +0900

    [LeetCode#207] CourseSchedule

--- codingInterviewBook/datastructure/non_linear/RunSolution.java
+++ codingInterviewBook/datastructure/non_linear/RunSolution.java
@@ -1,11 +1,13 @@
 package datastructure.non_linear;
 
+import datastructure.non_linear.graph.CourseSchedule;
 import datastructure.non_linear.graph.TravelRoute;
 
 public class RunSolution {
     public static void main(String[] args) {
-        TravelRoute travelRoute = new TravelRoute();
-        String[][] tickets = {{"ICN", "A"}, {"ICN", "B"}, {"B", "C"}, {"A", "C"}};
-        travelRoute.solution(tickets);
+        CourseSchedule courseSchedule = new CourseSchedule();
+        int[][] prerequisites = {{1,0}, {2,1}};
+        boolean answer = courseSchedule.canFinish(3, prerequisites);
+        System.out.println(answer);
     }
 }
---
+++ codingInterviewBook/datastructure/non_linear/graph/CourseSchedule.java
@@ -0,0 +1,46 @@
+package datastructure.non_linear.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CourseSchedule {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> finishToTakeMap = new HashMap<>();
+        for (int[] prerequisite : prerequisites) {
+            finishToTakeMap.putIfAbsent(prerequisite[0], new ArrayList<>());
+            finishToTakeMap.get(prerequisite[0]).add(prerequisite[1]);
+        }
+
+        List<Integer> takes = new ArrayList<>();
+        List<Integer> taken = new ArrayList<>();
+        for (Integer finish : finishToTakeMap.keySet()) {
+            if (!dfs(finishToTakeMap, finish, takes, taken)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean dfs(Map<Integer, List<Integer>> finishToTakeMap, Integer finish, List<Integer> takes, List<Integer> taken) {
+        if (takes.contains(finish)) {
+            return false;
+        }
+        if (taken.contains(finish)) {
+            return true;
+        }
+        if (finishToTakeMap.containsKey(finish)) {
+            takes.add(finish);
+            for (Integer take : finishToTakeMap.get(finish)) {
+                if (!dfs(finishToTakeMap, take, takes, taken)) {
+                    return false;
+                }
+            }
+            takes.remove(finish);
+            taken.add(finish);
+        }
+        return true;
+    }
+}
