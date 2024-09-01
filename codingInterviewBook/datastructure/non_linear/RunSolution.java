d9ae45b2ee1c43f85b72a267b03f2001ed485a0a
gilbert9172 <gilbert9172@naver.com>
2024-09-01 18:04:12 +0900

    runsolution 초기화

--- codingInterviewBook/datastructure/non_linear/RunSolution.java
+++ codingInterviewBook/datastructure/non_linear/RunSolution.java
@@ -4,9 +4,6 @@ import datastructure.non_linear.graph.courseSchedule.V1;
 
 public class RunSolution {
     public static void main(String[] args) {
-        V1 courseSchedule = new V1();
-        int[][] prerequisites = {{1,0}, {2,1}};
-        boolean answer = courseSchedule.canFinish(3, prerequisites);
-        System.out.println(answer);
+
     }
 }
