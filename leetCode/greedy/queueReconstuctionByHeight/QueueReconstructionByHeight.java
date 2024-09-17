41c8c8d82986e10321086922ec456992ec2d6518
gilbert9172 <gilbert9172@naver.com>
2024-09-17 16:58:45 +0900

    [LeetCode#406] Queue Reconstruction by Height

---
+++ leetCode/greedy/queueReconstuctionByHeight/QueueReconstructionByHeight.java
@@ -0,0 +1,22 @@
+package greedy.queueReconstuctionByHeight;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueReconstructionByHeight {
+    public int[][] reconstructQueue(int[][] people) {
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]);
+        for (int[] person : people) {
+            pq.offer(person);
+        }
+        List<int[]> result = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            int[] person = pq.poll();
+            result.add(person[1], person);
+        }
+        return result.toArray(new int[people.length][2]);
+    }
+
+}
---
+++ leetCode/greedy/queueReconstuctionByHeight/RunSolve.java
@@ -0,0 +1,9 @@
+package greedy.queueReconstuctionByHeight;
+
+public class RunSolve {
+    public static void main(String[] args) {
+        QueueReconstructionByHeight solution = new QueueReconstructionByHeight();
+        int[][] persons = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+        solution.reconstructQueue(persons);
+    }
+}
