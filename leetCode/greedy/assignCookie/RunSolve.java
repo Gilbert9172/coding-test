1f617509e7050234315f111eeb5ed795d6018e14
gilbert9172 <gilbert9172@naver.com>
2024-09-21 18:03:08 +0900

    [LeetCode#455] Assign Cookies

---
+++ leetCode/greedy/assignCookie/RunSolve.java
@@ -0,0 +1,15 @@
+package greedy.assignCookie;
+
+public class RunSolve {
+    public static void main(String[] args) {
+        V1 v1 = new V1();
+        int[] g = {10, 9, 8, 7};
+        int[] s = {5, 6, 7, 8};
+//        int[] g = {10, 9, 8, 7};
+//        int[] s = {10, 9, 8, 7};
+//        int[] g = {1 ,2 ,3};
+//        int[] s = {3};
+        int answer = v1.findContentChildren(g, s);
+        System.out.println(answer);
+    }
+}
