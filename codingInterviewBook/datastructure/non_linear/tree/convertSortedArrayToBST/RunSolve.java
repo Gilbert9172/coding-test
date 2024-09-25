8b558a786853c4d2d2b4b968d657a6cecacaaf5e
gilbert9172 <gilbert9172@naver.com>
2024-09-25 11:52:35 +0900

    [LeetCode#108] Convert Sorted Array to Binary Search Tree

---
+++ codingInterviewBook/datastructure/non_linear/tree/convertSortedArrayToBST/RunSolve.java
@@ -0,0 +1,9 @@
+package datastructure.non_linear.tree.convertSortedArrayToBST;
+
+public class RunSolve {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        V1 solution = new V1();
+        solution.sortedArrayToBST(nums);
+    }
+}
