71579e8b89583b3cae3635eeb66e7f3e8868951b
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2024-11-19 14:07:09 +0900

    [LeetCode#257] Binary Tree Paths

---
+++ leetCode/backTracking/easy/Quiz257.java
@@ -0,0 +1,45 @@
+package backTracking.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quiz257 {
+
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> answer = new ArrayList<>();
+        findRoute(root, "", answer);
+        return answer;
+    }
+
+    private void findRoute(TreeNode root, String path, List<String> answer) {
+        if (root.left == null && root.right == null) {
+            answer.add(path + root.val);
+        }
+
+        if (root.left != null) {
+            findRoute(root.left, path + root.val + "->", answer);
+        }
+
+        if (root.right != null) {
+            findRoute(root.right, path + root.val + "->", answer);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = setUp();
+        Quiz257 quiz257 = new Quiz257();
+        List<String> nodeValues = quiz257.binaryTreePaths(treeNode);
+        for (String nodeValue : nodeValues) {
+            System.out.println(nodeValue);
+        }
+    }
+
+    private static TreeNode setUp() {
+        TreeNode node5 = TreeNode.of(5, null, null);
+        TreeNode node4 = TreeNode.of(4, null, null);
+        TreeNode node3 = TreeNode.of(3, null, null);
+        TreeNode node2 = TreeNode.of(2, node4, node5);
+        return TreeNode.of(1, node2, node3);
+    }
+}
---
+++ leetCode/backTracking/easy/TreeNode.java
@@ -0,0 +1,29 @@
+package backTracking.easy;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public static TreeNode from(int val) {
+        return new TreeNode(val);
+    }
+
+    public static TreeNode of(int val, TreeNode left, TreeNode right) {
+        return new TreeNode(val, left, right);
+    }
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
