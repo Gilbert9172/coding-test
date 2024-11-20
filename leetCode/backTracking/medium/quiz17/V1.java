67cd1654761b98bcf21e3d09628571d0ed2aae96
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2024-11-20 13:39:28 +0900

    [LeetCode#17] Letter Combinations of a Phone Number

---
+++ leetCode/backTracking/medium/quiz17/V1.java
@@ -0,0 +1,52 @@
+package backTracking.medium.quiz17;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class V1 {
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits.isEmpty()) {
+            return result;
+        }
+        Map<Character, String> characterMap = initiateMap();
+        StringBuilder currentCombination = new StringBuilder();
+        backTracking(digits, 0, characterMap, currentCombination, result);
+        return result;
+    }
+
+    private void backTracking(
+            String digits,
+            int idx,
+            Map<Character, String> characterMap,
+            StringBuilder builder,
+            List<String> result
+    ) {
+        if (idx == digits.length()) {
+            result.add(builder.toString());
+            return;
+        }
+        char digit = digits.charAt(idx);
+        String letters = characterMap.get(digit);
+        for (char letter : letters.toCharArray()) {
+            builder.append(letter);
+            backTracking(digits, idx + 1, characterMap, builder, result);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+    }
+
+    private Map<Character, String> initiateMap() {
+        return Map.of(
+                '2', "abc",
+                '3', "def",
+                '4', "ghi",
+                '5', "jkl",
+                '6', "mno",
+                '7', "pqrs",
+                '8', "tuv",
+                '9', "wxyz"
+        );
+    }
+}
