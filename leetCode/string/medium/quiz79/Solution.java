e3dc0c6cbc1d7674d19abc1d4d8fb97b9214cb98
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2024-12-16 15:35:19 +0900

    [LeetCode#79] Word Search

---
+++ leetCode/string/medium/quiz79/Solution.java
@@ -0,0 +1,76 @@
+package string.medium.quiz79;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        int maxRow = board.length;
+        int maxCol = board[0].length;
+
+        for (int row = 0; row < maxRow; row++) {
+            for (int col = 0; col < maxCol; col++) {
+                if (board[row][col] == word.charAt(0)) {
+                    boolean[][] visited = new boolean[maxRow][maxCol];
+                    visited[row][col] = true;
+                    List<String> bucket = new ArrayList<>();
+                    bucket.add(Character.toString(board[row][col]));
+                    String answer = backTracking(board, visited, 1, row, col, word, bucket);
+                    if (answer.equals(word)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public String backTracking(char[][] board, boolean[][] visited, int letterCount, int row, int col, String word, List<String> bucket) {
+        int maxRow = board.length;
+        int maxCol = board[0].length;
+
+        if (letterCount == word.length()) {
+            return String.join("", bucket);
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int[] dir : dirs) {
+            int nextRow = row + dir[0];
+            int nextCol = col + dir[1];
+
+            if (nextRow < 0 || nextCol < 0 || nextRow >= maxRow || nextCol >= maxCol) {
+                continue;
+            }
+
+            if (board[nextRow][nextCol] == word.charAt(letterCount) && !visited[nextRow][nextCol]) {
+                bucket.add(Character.toString(board[nextRow][nextCol]));
+                visited[nextRow][nextCol] = true;
+                String string = backTracking(board, visited, letterCount + 1, nextRow, nextCol, word, bucket);
+                if (string.length() == word.length()) {
+                    return string;
+                }
+                bucket.removeLast();
+                visited[nextRow][nextCol] = false;
+
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String input = "ABCESEEEFS";
+        Solution solution = new Solution();
+        char[][] board = {
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'E', 'S'},
+                {'A', 'D', 'E', 'E'}
+        };
+//        char[][] board = {
+//                {'a', 'a'}
+//        };
+
+
+        boolean exist = solution.exist(board, input);
+        System.out.println(exist);
+    }
+}
