7975f3ec7862e0f1d23e44ceaf27104cbcf679c8
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2025-01-22 12:44:41 +0900

    [LeetCode#11] Container With Most Water 백준 난이도 : 골드Ⅴ~Ⅲ

---
+++ leetCode/twoPointer/quiz11/Solution.java
@@ -0,0 +1,34 @@
+package twoPointer.quiz11;
+
+public class Solution {
+
+    public int maxArea(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int maxArea = Integer.MIN_VALUE;
+
+        while (left < right) {
+            int area = calculate(left, right, heights);
+            maxArea = Math.max(area, maxArea);
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    private int calculate(int left, int right, int[] heights) {
+        int bottom = right - left;
+        int height = Math.min(heights[right], heights[left]);
+        return bottom * height;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] heights = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int answer = solution.maxArea(heights);
+        System.out.println(answer);
+    }
+}
