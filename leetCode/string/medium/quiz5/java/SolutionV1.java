ed1e16de1914e4d949add02e7bf01b5799f7746a
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2025-04-18 23:33:51 +0900

    5. Longest Palindromic Substring

--- leetCode/string/medium/quiz5/java/SolutionV1.java
+++ leetCode/string/medium/quiz5/java/SolutionV1.java
@@ -1,7 +1,11 @@
-package multiDemensionDP.medium.quiz5;
+package string.medium.quiz5.java;
 
 // Manacher 알고리즘
-public class Manacher {
+public class SolutionV1 {
+
+    private int[] radiuses;
+    private int inputLen;
+
     public String longestPalindrome(String s) {
         // 1. 문자열 변환
         StringBuilder builder = new StringBuilder();
@@ -12,8 +16,8 @@ public class Manacher {
         String transformedInput = builder.toString();
 
         // 2. 변수 초기화
-        int inputLen = transformedInput.length();
-        int[] radiuses = new int[inputLen]; // 팰린드롬 반지름 배열
+        inputLen = transformedInput.length();
+        radiuses = new int[inputLen]; // 팰린드롬 반지름 배열
         int currentCenterIdx = 0, rightBoundary = 0; // 중심과 오른쪽 경계
         int maxLen = 0, centerIdxOfMaxPalindrome = 0;
 
@@ -25,15 +29,15 @@ public class Manacher {
             }
 
             // 4. 팰린드롬 확장
-            while (i + radiuses[i] + 1 < inputLen && i - (radiuses[i] + 1) >= 0 &&
-                    transformedInput.charAt(i + radiuses[i] + 1) == transformedInput.charAt(i - (radiuses[i] + 1))) {
+            while (isValidRange(i) && isSameAlphabet(transformedInput, i)) {
                 radiuses[i]++;
             }
 
             // 5. 중심과 오른쪽 경계 업데이트
-            if (i + radiuses[i] > rightBoundary) {
+            int currentMaxBoundary = i + radiuses[i];
+            if (currentMaxBoundary > rightBoundary) {
                 currentCenterIdx = i;
-                rightBoundary = i + radiuses[i];
+                rightBoundary = currentMaxBoundary;
             }
 
             // 6. 최대 길이 갱신
@@ -48,10 +52,18 @@ public class Manacher {
         return s.substring(start, start + maxLen);
     }
 
+    private boolean isValidRange(int idx) {
+        return (idx + (radiuses[idx] + 1) < inputLen) && (idx - (radiuses[idx] + 1) >= 0);
+    }
+
+    private boolean isSameAlphabet(String s, int idx) {
+        return s.charAt(idx - (radiuses[idx] + 1)) == s.charAt(idx + radiuses[idx] + 1);
+    }
+
     public static void main(String[] args) {
-        String input = "xxx";
-        Manacher solutionV2 = new Manacher();
-        String answer = solutionV2.longestPalindrome(input);
+        String input = "babad";
+        SolutionV1 solution = new SolutionV1();
+        String answer = solution.longestPalindrome(input);
         System.out.println(answer);
     }
 }
